2dd7d5aeec9ed5a6994009dae6024447a86c0d6e
George Kachambwa <kachambwa.george@gmail.com>
2015-06-15 12:28:42 +0200

    Revert "angular dependancy updgrade"
    This reverts commit 267b89cba22395d6dceaa534a40b30a7f08751e0.

--- public/modules/articles/controllers/articles.client.controller.js
+++ public/modules/articles/controllers/articles.client.controller.js
@@ -1,31 +1,8 @@
 'use strict';
 
-angular.module('articles').controller('ArticlesController', ['$scope', '$stateParams', '$location', 'Authentication', 'Articles','$mdToast','$animate',
-	function($scope, $stateParams, $location, Authentication, Articles,$mdToast,$animate) {
+angular.module('articles').controller('ArticlesController', ['$scope', '$stateParams', '$location', 'Authentication', 'Articles',
+	function($scope, $stateParams, $location, Authentication, Articles) {
 		$scope.authentication = Authentication;
-	//toastr
-	$scope.toastPosition = {
-		bottom: false,
-		top: true,
-		left: true,
-		right: false
-	};
-	
-	$scope.getToastPosition = function(){
-		return Object.keys($scope.toastPosition)
-		.filter(function (pos) {
-			return $scope.toastPosition[pos];
-		}).join(' ');
-	};
-	
-	$scope.sendMail = function(){
-		$mdToast.show(
-			$mdToast.simple()
-			.content('Thanks for your message, Youre the man')
-			.hideDelay(3000)
-	
-		);
-	};
 
 		$scope.create = function() {
 			var article = new Articles({
